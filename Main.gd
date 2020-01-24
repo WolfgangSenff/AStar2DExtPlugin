e1f9427c4d16a87d3a987d6f651af60598bb23d3
Kyle Szklenski <kyle.szklenski@gmail.com>
2020-01-24 07:48:18 -0500

    Quick update to add call-deferred for weights

--- Main.gd
+++ Main.gd
@@ -9,9 +9,9 @@ func _ready() -> void:
     astar2dext.build_astar2d_map()
     astar2dext.full_update_indicator_colors_for_weight()
     # This is just for debugging. You'd want to do this manually, for instance in the ready func of your object
-    for idx in mountain_randomize_iterations:
-        for mountain in mountains.get_children():
-            var weight_at_position = astar2dext.get_astar_weight_at_pos(mountain.global_position)
-            var weight_child = mountain.get_node("AStar2DWeight")
-            weight_child.set_weight(weight_at_position + (randi() % int(rand_range(1, 100))))
-    
+#    for idx in mountain_randomize_iterations:
+#        for mountain in mountains.get_children():
+#            var weight_at_position = astar2dext.get_astar_weight_at_pos(mountain.global_position)
+#            var weight_child = mountain.get_node("AStar2DWeight")
+#            weight_child.set_weight(weight_at_position + (randi() % int(rand_range(1, 100))))
+#
--- addons/astar2d_ext/AStar2DWeight.gd
+++ addons/astar2d_ext/AStar2DWeight.gd
@@ -4,13 +4,18 @@ const GroupName = "astar2d_ext"
 
 var astar2d_map
 
-export (bool) var ShouldDisableTile = false
+export (bool) onready var ShouldDisableTile = false
+export (float) onready var Weight
 
 func _ready() -> void:
     astar2d_map = get_tree().get_nodes_in_group(GroupName)[0]
+    call_deferred("set_weight")
 
-func set_weight(value):
+func set_weight(value = null):
     if ShouldDisableTile:
         astar2d_map.disable_astar_node_at_position(global_position)
     else:
-        astar2d_map.update_astar_weight_at_position(global_position, value)
+        if value:
+            astar2d_map.update_astar_weight_at_position(global_position, value)
+        else:
+            astar2d_map.update_astar_weight_at_position(global_position, Weight)
