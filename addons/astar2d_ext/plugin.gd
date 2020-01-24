851eef8362aad066b79c9fbd3578d67f841703a3
Kyle Szklenski <kyle.szklenski@gmail.com>
2020-01-24 06:42:54 -0500

    Quick update for new nodes

--- addons/astar2d_ext/plugin.gd
+++ addons/astar2d_ext/plugin.gd
@@ -3,7 +3,11 @@ extends EditorPlugin
 
 func _enter_tree():
     add_custom_type("AStar2DExt", "Node2D", preload("res://addons/astar2d_ext/AStar2DExt.gd"), preload("res://addons/astar2d_ext/icon.png"))
+    add_custom_type("AStar2DWeight", "Node2D", preload("res://addons/astar2d_ext/AStar2DWeight.gd"), preload("res://addons/astar2d_ext/icon.png"))
+    add_custom_type("AStar2DCharacter", "Node2D", preload("res://addons/astar2d_ext/AStar2DCharacter.gd"), preload("res://addons/astar2d_ext/icon.png"))
  
 func _exit_tree():
     remove_custom_type("AStar2DExt")
+    remove_custom_type("AStar2DWeight")
+    remove_custom_type("AStar2DCharacter")
  
