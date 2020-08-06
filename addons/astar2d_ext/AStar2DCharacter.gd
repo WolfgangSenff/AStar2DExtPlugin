68c7e3e62341a7e46e11eceb5ef6a0a16b7fa646
Kyle Szklenski <kyle.szklenski@gmail.com>
2020-08-06 16:07:11 -0400

    Fix bug where previous_weight was not recorded

--- addons/astar2d_ext/AStar2DCharacter.gd
+++ addons/astar2d_ext/AStar2DCharacter.gd
@@ -16,27 +16,22 @@ const GroupName = "astar2d_ext"
 func _ready():
     astar2d_map = get_tree().get_nodes_in_group(GroupName)[0]
 
-func disable_astar_position():
+func disable_astar_position(pos):
     var tree = get_tree()
     if tree:
-        previous_weight = tree.call_group(GroupName, "get_astar_weight_at_pos", global_position)
+        previous_weight = astar2d_map.get_astar_weight_at_pos(pos)
         
         if CanBePassedThrough:
-            tree.call_group(GroupName, "update_astar_weight_at_position", global_position, Weight)
+            astar2d_map.update_astar_weight_at_position(pos, Weight)
         else:
-            tree.call_group(GroupName, "disable_astar_node_at_position", global_position)
-            
-func restore_astar_position():
+            astar2d_map.disable_astar_node_at_position(pos)
+        
+func restore_astar_position(pos):
     var tree = get_tree()
     if tree:
         if CanBePassedThrough and previous_weight:
-            tree.call_group(GroupName, "update_astar_weight_at_position", global_position, previous_weight)
+            astar2d_map.update_astar_weight_at_position(pos, previous_weight)
         else:
-            tree.call_group(GroupName, "enable_astar_node_at_position", global_position)
+            astar2d_map.enable_astar_node_at_position(pos)
             
         previous_weight = null
-
-func get_astar_path_to_pos(pos : Vector2) -> PoolVector2Array:
-    # Get position path through graph to be able to navigate through it
-    var result = astar2d_map.get_astar_path_to_pos(global_position + (astar2d_map.tilemap.cell_size / 2.0), pos)
-    return result
